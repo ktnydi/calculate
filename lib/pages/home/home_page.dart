866d1830143449af9bcc3b88c145fbfa4471f6ec
ktnydi <kitano.prog@gmail.com>
2021-06-17 18:53:23 +0900

    Fix: ホーム画面レイアウトを変更

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:app_review/app_review.dart';
 import 'package:calculate/analytics.dart';
 import 'package:calculate/enums/preference.dart';
@@ -124,107 +126,121 @@ class Home extends ConsumerWidget {
 
         return Scaffold(
           backgroundColor: Theme.of(context).colorScheme.primary,
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(width: 2),
-                      bottom: BorderSide(width: 2),
-                    ),
-                  ),
-                  child: Text(
-                    'Keisan Doriru',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 32),
-                Text(
-                  '＼ Let\'s challenge! ／',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                const SizedBox(height: 32),
-                Center(
-                  child: ElevatedButton(
-                    child: Text(
-                      'スタート\n（${quizType.name}・${quizType == QuizType.numQuizzes ? '$quizLength問' : '$limit秒'}）',
-                      textAlign: TextAlign.center,
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
+          body: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height -
+                    (bannerAd?.size.height.toDouble() ?? 0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/blobs.png',
+                        width: min(MediaQuery.of(context).size.width, 320),
+                        color: Theme.of(context).colorScheme.primaryVariant,
                       ),
-                      minimumSize: Size(232, 80),
-                      primary: Colors.white,
-                      onPrimary: Theme.of(context).colorScheme.primaryVariant,
-                    ),
-                    onPressed: () {
-                      analytics.logStartGame();
-                      context.refresh(randomQuizProvider);
-                      context.refresh(quizProvider);
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          fullscreenDialog: true,
-                          builder: (context) => Game(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ElevatedButton(
-                      child: Text('設定'),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        minimumSize: Size(112, 56),
-                        primary: Colors.white,
-                        onPrimary: Theme.of(context).colorScheme.primaryVariant,
-                      ),
-                      onPressed: () async {
-                        await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Setting(),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Keisan Doriru',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        );
-                        context.refresh(sharedPreferencesProvider);
-                      },
-                    ),
-                    const SizedBox(width: 8),
-                    ElevatedButton(
-                      child: Text('ヘルプ'),
+                          const SizedBox(height: 32),
+                          Text(
+                            'Let\'s challenge!',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  Center(
+                    child: ElevatedButton(
+                      child: Text(
+                        'スタート\n（${quizType.name}・${quizType == QuizType.numQuizzes ? '$quizLength問' : '$limit秒'}）',
+                        textAlign: TextAlign.center,
+                      ),
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16),
                         ),
-                        minimumSize: Size(112, 56),
+                        minimumSize: Size(232, 80),
                         primary: Colors.white,
                         onPrimary: Theme.of(context).colorScheme.primaryVariant,
                       ),
                       onPressed: () {
-                        Navigator.push(
+                        analytics.logStartGame();
+                        context.refresh(randomQuizProvider);
+                        context.refresh(quizProvider);
+                        Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => Help(),
+                            fullscreenDialog: true,
+                            builder: (context) => Game(),
                           ),
                         );
                       },
                     ),
-                  ],
-                )
-              ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        child: Text('設定'),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          minimumSize: Size(112, 56),
+                          primary: Colors.white,
+                          onPrimary:
+                              Theme.of(context).colorScheme.primaryVariant,
+                        ),
+                        onPressed: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Setting(),
+                            ),
+                          );
+                          context.refresh(sharedPreferencesProvider);
+                        },
+                      ),
+                      const SizedBox(width: 8),
+                      ElevatedButton(
+                        child: Text('ヘルプ'),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          minimumSize: Size(112, 56),
+                          primary: Colors.white,
+                          onPrimary:
+                              Theme.of(context).colorScheme.primaryVariant,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => Help(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
           bottomNavigationBar: bannerAd != null
