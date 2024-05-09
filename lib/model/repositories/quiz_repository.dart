201ceb0f4e48a17b62e3a868bf945a2d5039ebd1
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2024-05-10 00:38:33 +0900

    refactor: 整数値をランダムで1つ取得する処理を変更

--- lib/model/repositories/quiz_repository.dart
+++ lib/model/repositories/quiz_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:calculate/model/domains/quiz/quiz.dart';
 import 'package:calculate/enums/quiz_category.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,34 +7,35 @@ final quizRepositoryProvider = Provider<QuizRepository>(
 );
 
 class QuizRepository {
+  /// [start]から[last]の範囲内でランダムに整数を1つ取得する。
+  int _getRandamIntInRange(int start, int last) {
+    assert(start < last);
+    assert(start > 0);
+    assert(last > 0);
+
+    final rangeValue = List.generate(
+      last + 1 - start,
+      (index) => start + index,
+    );
+    return [...rangeValue..shuffle()].first;
+  }
+
   Quiz getAdditional() {
-    final random = Random();
-    const minNum = 10;
-    const maxNum = 100;
-    createNum(int min, int max) => min + random.nextInt(max - min);
-    final num1 = createNum(minNum, maxNum);
-    final num2 = createNum(minNum, maxNum);
+    final num1 = _getRandamIntInRange(10, 99);
+    final num2 = _getRandamIntInRange(10, 99);
     final figures = [num1, num2];
     return Quiz(figures: figures, type: QuizCategory.additional);
   }
 
   Quiz getSubtraction() {
-    final random = Random();
-    const minNum = 10;
-    const maxNum = 100;
-    createNum(int min, int max) => min + random.nextInt(max - min);
-    final num1 = createNum(minNum, maxNum);
-    final num2 = createNum(minNum, maxNum);
+    final num1 = _getRandamIntInRange(10, 99);
+    final num2 = _getRandamIntInRange(10, 99);
     final figures = num1 > num2 ? [num1, num2] : [num2, num1];
     return Quiz(figures: figures, type: QuizCategory.subtraction);
   }
 
   Quiz getDivision() {
-    final random = Random();
-    const minNum = 10;
-    const maxNum = 100;
-    createNum(int min, int max) => min + random.nextInt(max - min);
-    final num1 = createNum(minNum, maxNum);
+    final num1 = _getRandamIntInRange(10, 99);
 
     /// num1の約数をランダムで1つ取得する
     final divisors = <int>[];
@@ -53,12 +52,9 @@ class QuizRepository {
   }
 
   Quiz getMultiplication() {
-    final random = Random();
-    const minNum = 0;
-    const maxNum = 10;
-    createNum(int min, int max) => min + random.nextInt(max - min);
-    final num1 = createNum(minNum, maxNum);
-    final num2 = createNum(minNum, maxNum);
+    final num1 = _getRandamIntInRange(1, 9);
+    final num2 = _getRandamIntInRange(1, 9);
+
     final figures = [num1, num2];
     return Quiz(figures: figures, type: QuizCategory.multiplication);
   }
