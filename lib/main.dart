7f1621e90be8c8e21803e4ed6eb8364ed2735602
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2024-06-01 00:18:14 +0900

    chore: use `package_info_plus`

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
--- lib/providers/update_request_provider.dart
+++ lib/providers/update_request_provider.dart
@@ -4,7 +4,7 @@ import 'package:calculate/model/domains/update_info/update_info.dart';
 import 'package:calculate/enums/update_request_type.dart';
 import 'package:calculate/model/repositories/remote_config_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:version/version.dart';
 
 final updateRequestProvider = FutureProvider<UpdateRequestType>(
