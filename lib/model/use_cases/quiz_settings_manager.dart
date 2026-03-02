3b287b1776ad3754c5c02ef622e7fd0b00305ef2
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2026-03-03 07:15:58 +0900

    feat: 問題設定データの保存形式を変更

--- lib/model/use_cases/quiz_settings_manager.dart
+++ lib/model/use_cases/quiz_settings_manager.dart
@@ -1,11 +1,21 @@
 import 'dart:convert';
 
+import 'package:calculate/enums/quiz_category.dart';
 import 'package:calculate/enums/quiz_category_mode.dart';
 import 'package:calculate/model/domains/quiz_settings/quiz_settings.dart';
 import 'package:calculate/model/domains/quiz_settings/term_settings.dart';
 import 'package:calculate/providers/shared_preferences_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+enum QuizSettingsKey {
+  mode,
+  quantity,
+  term;
+
+  const QuizSettingsKey();
+}
 
 final quizSettingsManagerNotifierProvider =
     StateNotifierProvider.autoDispose<
@@ -17,33 +27,64 @@ final quizSettingsManagerNotifierProvider =
 
 class QuizSettingsManagerNotifier extends StateNotifier<QuizSettings> {
   QuizSettingsManagerNotifier(this.ref) : super(QuizSettings()) {
-    final quizSettingsString = prefs.getString(key);
-    if (quizSettingsString == null) return;
-    final json = jsonDecode(quizSettingsString) as Map<String, dynamic>;
-    state = QuizSettings.fromJson(json);
+    initialize();
   }
 
   final Ref ref;
-  final key = 'quiz_settings.json';
 
   SharedPreferences get prefs => ref.read(sharedPreferencesProvider);
+
+  void initialize() {
+    final modeNames = prefs.getStringList(QuizSettingsKey.mode.name);
+    final quantity = prefs.getInt(QuizSettingsKey.quantity.name);
+    final term = prefs.getString(QuizSettingsKey.term.name);
+
+    if (modeNames != null) {
+      final modes = modeNames
+          .map((value) => QuizCategory.values.byName(value))
+          .toList();
+
+      final category = QuizCategoryMode.values.firstWhere(
+        (value) => listEquals(value.categories, modes),
+      );
+
+      state = state.copyWith(category: category);
+    }
+
+    if (quantity != null) {
+      state = state.copyWith(size: quantity);
+    }
+
+    if (term != null) {
+      final termJson = jsonDecode(term) as Map<String, dynamic>;
+
+      state = state.copyWith(term: Term.fromJson(termJson));
+    }
+  }
 
   Future<void> updateQuizCategory(QuizCategoryMode category) async {
     final newState = state.copyWith(category: category);
 
     state = newState;
-    await prefs.setString(key, jsonEncode(newState.toJson()));
+    await prefs.setStringList(
+      QuizSettingsKey.mode.name,
+      category.categories.map((value) => value.name).toList(),
+    );
   }
 
   Future<void> updateQuizSize(int size) async {
     final newState = state.copyWith(size: size);
 
     state = newState;
-    await prefs.setString(key, jsonEncode(newState.toJson()));
+    await prefs.setInt(QuizSettingsKey.quantity.name, size);
   }
 
   Future<void> updateTerm(Term term) async {
     state = state.copyWith(term: term);
-    await prefs.setString(key, jsonEncode(state.copyWith(term: term)));
+
+    await prefs.setString(
+      QuizSettingsKey.term.name,
+      jsonEncode(term.toJson()),
+    );
   }
 }
